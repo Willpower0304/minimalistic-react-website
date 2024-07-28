9880a12d42b2d512f1029c4f66678d518abf3266
William Espinoza <espinozaw657@gmail.com>
2024-07-27 22:39:00 -0500

    feat: sticky navbar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,8 +3,8 @@ import styles from "./Hero.module.css";
 
 export const Hero = () => {
   return (
-    <section className={styles.container}>
-      <h1 className={styles.title}>Minimalism</h1>
+    <section className={styles.container} id="Home">
+      <h1 className={styles.title}>MINIMALISM</h1>
       <h4 className={styles.slogan}>is a way of life</h4>
     </section>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Navbar.module.css";
 
 import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsSticky(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <nav className={styles.container} id="Home">
+    <nav className={`${styles.container} ${isSticky ? styles.sticky : ""}`}>
       <div className={styles.leftcontainer}>
-        <ul>
-          <a href="">
-            <img
-              className={styles.imglogo}
-              src={getImageUrl("nav/logo.svg")}
-              alt="logo"
-            />
-          </a>
+        <a href="">
+          <img
+            className={styles.imglogo}
+            src={getImageUrl("nav/logo.svg")}
+            alt="logo"
+          />
+        </a>
+      </div>
+      <div className={styles.rightcontainer}>
+        <i
+          className={menuOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars"}
+          onClick={() => setMenuOpen(!menuOpen)}
+        ></i>
+        <ul
+          className={`${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(true)}
+        >
           <li>
             <a href="#Home">HOME</a>
           </li>
@@ -27,12 +48,6 @@ export const Navbar = () => {
             <a href="#Contact">CONTACT</a>
           </li>
         </ul>
-      </div>
-      <div className={styles.rightcontainer}>
-        <i
-          className={menuOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars"}
-          onClick={() => setMenuOpen(!menuOpen)}
-        ></i>
       </div>
     </nav>
   );
